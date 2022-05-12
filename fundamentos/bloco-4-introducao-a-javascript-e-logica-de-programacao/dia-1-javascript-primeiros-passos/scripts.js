68d9d38de98290a6405b08744f278781350a7b67
HumbertoAraujo <humbertovt@yahoo.com.br>
2022-05-12 15:17:33 -0300

    Vamos Praticar Vaiáveis

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
@@ -0,0 +1,10 @@
+const myName = "Humberto"
+const birthCity = "São Leopoldo"
+let birthYear = 1985;
+console.log("Meu nome é:", myName,", Nasci em: ", birthCity, "no ano de: ", birthYear);
+birthYear = 2030;
+console.log("Meu nome é:", myName,", Nasci em: ", birthCity, "no ano de: ", birthYear);
+birthCity = "São Paulo" // Erro, pois não é possível alterar dado de uma constante
+
+
+
