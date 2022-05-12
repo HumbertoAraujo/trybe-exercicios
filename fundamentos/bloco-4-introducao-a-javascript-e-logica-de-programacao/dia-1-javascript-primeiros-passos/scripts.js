939a15af63c50c51a63c43a9793dd078c79f5916
HumbertoAraujo <humbertovt@yahoo.com.br>
2022-05-12 19:14:06 -0300

    Vamos Praticar Switch Case

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
@@ -1,76 +1,22 @@
-// const comida = 'pão na chapa';
-// const bebida = 'cafezinho';
-
-// if (bebida === 'Refrigerante' && comida === 'pão na chapa') {
-//   console.log('Muito obrigado pela refeição :)');
-// } else {
-//   console.log('Acho que houve um engano com meu pedido');
-// }
-
-// const conditionOne = true;
-// const conditionTwo = false;
-
-// console.log(conditionOne && conditionTwo);
-// console.log(conditionOne || conditionTwo);
-// console.log(conditionOne && !conditionTwo);
-
-// const cenouras = true;
-// const leite = true;
-// const arroz = true;
-// const feijao = true;
-
-// const listaDeCompras = cenouras && leite && arroz && feijao;
-// console.log(listaDeCompras);
-
-// console.log(true && true); // true
-// console.log(true && false); // false
-// console.log(false && true); // false
-// console.log(false && false); // false
-
-// const currentHour = 10;
-// let message = "";
-
-// if (currentHour >= 22){
-//     message = "Não deveríamos comer nada, é hora de dormir";
-// }else if (currentHour >= 18 && currentHour <= 22){
-//     message = "Rango da noite, vamos jantar :D";
-// } else if (currentHour >= 14 && currentHour <= 18) {
-//     message = "Vamos fazer um bolo pro café da tarde?";
-// } else if (currentHour >= 11 && currentHour <= 14){
-//     message = "Hora do almoço!!!";
-// }else if (currentHour >= 4 && currentHour <= 11){
-//     message = "Hmmm, cheiro de café recém passado";
-// }
-// console.log(message);
-
-// const bebidaPrincipal =  'cafezinho';
-// const bebidaAlternativa = 'suco de laranja';
-
-// if (bebidaPrincipal === 'cafezinho' || bebidaAlternativa === 'suco de laranja') {
-//   console.log("Obrigado por me atender :D")
-// } else {
-//   console.log("Ei, eu não pedi isso!");
-// }
-// console.log(true || true); // true
-// console.log(true || false); // true
-// console.log(false || true); // true
-// console.log(false || false); // false
-// let weekDay = "Sábado";
-// if (weekDay === "segunda-feira" || weekDay === "terça-feira" || weekDay === "quarta-feira" || weekDay === "quinta-feira" || weekDay === "sexta-feira"){
-//     console.log("Oba, mais um dia de aprendizado na Trybe >:D");
-// } else {
-//     console.log("FINALMENTE, descanso merecido UwU");
-// }
-// console.log((2 + 2) === 4);
-// console.log(!(2 + 2) === 4);
-// const squirtle = "melhor pokemon inicial";
-
-// console.log(!squirtle); // false
-
-// console.log(!42); // false
-
-// console.log(!0); // true
-
-// console.log(!null); // true
-
-console.log(!undefined); // true
+const nota = -1;
+let result = "";
+if (nota >= 80 && nota <= 100) {
+    result = 'aprovada';
+} else if (nota >= 60 && nota < 80) {
+    result = 'lista';
+}else if (nota >= 0 && nota < 60){
+    result = 'reprovada';
+} 
+switch (result) {
+    case 'aprovada':
+        console.log('aprovada');
+        break;
+    case 'lista':
+        console.log('lista');
+        break;
+    case 'reprovada':
+        console.log('reprovada');
+        break;
+    default:
+        console.log('Nota inválida / Corrigir')
+}
