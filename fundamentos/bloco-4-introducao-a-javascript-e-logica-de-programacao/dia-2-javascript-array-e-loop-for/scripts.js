32682bf322a70f4033e2fa25bec8bca89b97adb1
HumbertoAraujo <humbertovt@yahoo.com.br>
2022-05-13 17:21:48 -0300

    Exercícios 1 á 9 ok

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
@@ -1,14 +1,85 @@
+
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
 //Exercício 1
-// let numeros = [1,2,3,4,5];
-// for(let numero of numeros) {
-//   console.log(numero);
+// for (let number of numbers ){
+//     console.log(number);
 // }
-// let arrOfNumbers = [10, 20, 30];
-// for (let sum of arrOfNumbers) {
-//   sum += 1;
-//   console.log(sum);
+
+//Exercício 2
+// let soma = 0;
+// for (let number of numbers){
+//     soma += number;
 // }
-let names = ['João', 'Maria', 'Antônio', 'Margarida'];
-for (let name of names){
-    console.log(name);
-}
+// console.log(soma);
+
+//Exercício 3
+// let soma = 0;
+// for (let number of numbers){
+//     soma += number;
+// }
+// let media = soma / numbers.length;
+// console.log(media);
+
+//Exercício 4
+// let soma = 0;
+// for (let number of numbers){
+//     soma += number;
+// }
+// let media = soma / numbers.length;
+// if (media > 20){
+//     console.log("valor maior que 20")
+// } else {
+//     console.log("valor menor ou igual a 20");
+// }
+//Exercício 5
+// let aux = 0;
+// for (number of numbers){
+//     if (number > aux) {
+//         aux = number
+//     }
+// }
+// console.log(aux);
+
+//Exercício 6
+// let qtdImpar = 0;
+
+// for (number of numbers){
+//     if (number % 2 != 0){
+//         qtdImpar += 1
+//     }
+// }
+// if (qtdImpar > 0) {
+//     console.log(qtdImpar);
+// } else {
+//     console.log("nenhum valor ímpar encontrado");
+// }
+
+//Exercício 7
+
+// let aux = 100000000000000000000000000000000000000000000000000000000000000000000;
+// for (number of numbers){
+//     if (number < aux) {
+//         aux = number
+//     }
+// }
+// console.log(aux);
+
+//Exercício 8
+
+// let newArray = [];
+// for (let index = 1; index <= 25; index += 1){
+//     newArray.push(index);
+// }
+// console.log(newArray);
+
+//Exercício 9
+
+// let newArray = [];
+// for (let index = 1; index <= 25; index += 1){
+//     newArray.push(index);
+// }
+// for (number of newArray){
+//     console.log(number / 2);
+// }
