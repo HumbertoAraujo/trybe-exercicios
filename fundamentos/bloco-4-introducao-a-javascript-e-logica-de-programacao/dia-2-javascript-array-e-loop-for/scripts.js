da2efe820d60adda46d7f3e3b0f5a1b2dfb4ec89
HumbertoAraujo <humbertovt@yahoo.com.br>
2022-05-13 16:26:15 -0300

    Exercícios for

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
@@ -1,17 +1,5 @@
-// //Exercício 1
-
-// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-// let menuServices = menu[1];
-
-// console.log(menuServices);
-// //Exercício 2
-
-// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-// let indexOfPortfolio = menu.indexOf('Portfólio');
-
-// console.log(indexOfPortfolio);
-// //Exercício 3
-
-// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-// menu.push('Contato');
-// console.log(menu);
+//Exercício 1
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+for (let index = 0; index < groceryList.length; index++ ) {
+    console.log(groceryList[index]);
+}
