cec3fbb2bbfd191daa7ee448c59e3a3bb0363a34
HumbertoAraujo <humbertovt@yahoo.com.br>
2022-05-16 11:59:28 -0300

    Exercícios dia 4.3 Exercício 3 ok

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/scripts.js
@@ -14,10 +14,27 @@
 // }
 // console.log(result);
 // Exercício 2
-let word = 'banana';
-let aux = '';
-for (let index = (word.length -1); index >= 0; index -= 1){
-    aux += word[index];
-}
-word = aux;
-console.log(word);
+// let word = 'banana';
+// let aux = '';
+// for (let index = (word.length -1); index >= 0; index -= 1){
+//     aux += word[index];
+// }
+// word = aux;
+// console.log(word);
+// Exercício 3:
+// let array = ['java', 'javascript', 'python', 'html', 'css'];
+// let maiorPalavra = '';
+// for (let palavra of array){
+//     if (palavra.length > maiorPalavra.length) {
+//         maiorPalavra = palavra
+//     }
+// }
+// console.log(maiorPalavra);
+// let array = ['java', 'javascript', 'python', 'html', 'css'];
+// let menorPalavra = array[0];
+// for (let palavra of array){
+//     if (palavra.length < menorPalavra.length) {
+//         menorPalavra = palavra
+//     }
+// }
+// console.log(menorPalavra);
