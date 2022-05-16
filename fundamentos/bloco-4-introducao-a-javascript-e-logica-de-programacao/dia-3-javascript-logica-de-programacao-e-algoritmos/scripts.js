3f1ca3f9eb80f31f97f53c5de88b8fcbb007823d
HumbertoAraujo <humbertovt@yahoo.com.br>
2022-05-16 11:46:14 -0300

    Exercícios dia 4.3 Exercício 2 ok

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/scripts.js
@@ -1,14 +1,23 @@
-const number = 10;
-let result = 0;
-let aux = 0;
-for (let index = number; index >= 1; index -= 1){
-    if (index - 1 != 0){
-        if (index == number){
-            aux += index * (index -1); 
-        } else {
-            result = aux * (index -1)
-            aux = result;
-        }
+// Exercício 1
+// const number = 10;
+// let result = 0;
+// let aux = 0;
+// for (let index = number; index >= 1; index -= 1){
+//     if (index - 1 != 0){
+//         if (index == number){
+//             aux += index * (index -1); 
+//         } else {
+//             result = aux * (index -1)
+//             aux = result;
+//         }
+// }
+// }
+// console.log(result);
+// Exercício 2
+let word = 'banana';
+let aux = '';
+for (let index = (word.length -1); index >= 0; index -= 1){
+    aux += word[index];
 }
-}
-console.log(result);
+word = aux;
+console.log(word);
