8ce7f009e7c5a1276ede64fc8ccba6a185560172
HumbertoAraujo <humbertovt@yahoo.com.br>
2022-05-17 19:03:36 -0300

    Para fixar dia 4.4 - for/in e for/of ok

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/scripts.js
@@ -89,10 +89,45 @@
 // console.log( "A jogadora " + player.name + "foi eleita a melhor do mundo por " + player.bestInTheWorld.length + " vezes.");
 // console.log( "A jogadora possui " + player.medals.golden + " medalhas de ouro e " + player.medals.silver + " medalhas de prata." );
 
+// let cars = ['Saab', 'Volvo', 'BMW'];
 
+// for (let index in cars) {
+//   console.log(cars[index]);
+// }
 
+// let car = {
+//     type: 'Fiat',
+//     model: '500',
+//     color: 'white',
+//   };
+  
+//   for (let index in car) {
+//     console.log(index, car[index]);
+//   }
 
-
-
+// let food = ['hamburguer', 'bife', 'acarajé'];
+// for (let position in food) {
+//     console.log(position);
+//   };
+// let food = ['hamburguer', 'bife', 'acarajé'];
+// for (let value of food) {
+//   console.log(value);
+// };
+// let names = {
+//     person1: 'João',
+//     person2: 'Maria',
+//     person3: 'Jorge',
+//   };
+//   for (let person in names){
+//       console.log('Olá ' + names[person]);
+//   }
+// let car = {
+//     model: 'A3 Sedan',
+//     manufacturer: 'Audi',
+//     year: 2020
+//   };
+// for (let key in car){
+//     console.log(key, car[key]);
+// }
 
 
