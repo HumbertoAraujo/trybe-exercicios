f223ae718d30da709fe579f04122c25af5ee9261
HumbertoAraujo <humbertovt@yahoo.com.br>
2022-05-17 16:22:04 -0300

    Para fixar dia 4.4 - Objetos ok

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/scripts.js
@@ -74,19 +74,20 @@
 // console.log(ultimoMorador.nome); 
 // console.log(moradores[moradores.length - 1].nome);
 // console.log(moradores[moradores.length - 1]['nome']);
-let player = {
-    name : 'Marta',
-    lastName : 'Silva',
-    age : 34,
-    medals : { 
-        golden: 2, 
-        silver: 3 
-    }, 
-    bestInTheWorld : [2006, 2007, 2008, 2009, 2010, 2018],
+// let player = {
+//     name : 'Marta',
+//     lastName : 'Silva',
+//     age : 34,
+//     medals : { 
+//         golden: 2, 
+//         silver: 3 
+//     }, 
+//     bestInTheWorld : [2006, 2007, 2008, 2009, 2010, 2018],
      
-}
-console.log( "A jogadora " + player.name + "tem " + player.age + " anos de idade.");
-console.log( "A jogadora " + player.name + "foi eleita a melhor do mundo por " + player.bestInTheWorld.length + " vezes.");
+// }
+// console.log( "A jogadora " + player.name + "tem " + player.age + " anos de idade.");
+// console.log( "A jogadora " + player.name + "foi eleita a melhor do mundo por " + player.bestInTheWorld.length + " vezes.");
+// console.log( "A jogadora possui " + player.medals.golden + " medalhas de ouro e " + player.medals.silver + " medalhas de prata." );
 
 
 
