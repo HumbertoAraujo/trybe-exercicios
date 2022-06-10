b5777dab0cece2b62a67d2ba65d120295e2988cc
HumbertoAraujo <humbertovt@yahoo.com.br>
2022-06-10 20:00:10 -0300

    Jest Tests examples

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/script.js
+++
@@ -1,6 +0,0 @@
-// EXEMPO DE TESTE SIMPLES COM ASSERT DO Node.JS
-// const assert = require('assert'); // Sintaxe para incluir o módulo assert
-
-// assert.strictEqual(50, 50); // Sem erros: 50 === 50
-// assert.strictEqual(50, 70); // AssertionError: 50 !== 70
-
