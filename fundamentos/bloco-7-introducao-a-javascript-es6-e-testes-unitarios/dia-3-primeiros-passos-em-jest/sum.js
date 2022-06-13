2c0b0311ca0e2f4a74e3002f6eaf5f720f276544
HumbertoAraujo <humbertovt@yahoo.com.br>
2022-06-13 15:32:33 -0300

    Exercício 5 OK

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.js
@@ -23,6 +23,59 @@ function myFizzBuzz(num) {
   return num;
 }
 
-// implemente seus testes aqui
+function encode(string) {
+  let newString = '';
+  for (let letter of string){
+    switch (letter) {
+      case 'a':
+        newString += '1';
+        break;
+      case 'e':
+        newString += '2';
+        break;
+      case 'i':
+        newString += '3';
+        break;
+      case 'o':
+        newString += '4';
+        break;
+      case 'u':
+          newString += '5';
+          break;
+      default :
+      newString += letter;
+    }
+  }
+  return newString;
+}
+function decode(string) {
+  let newString = '';
+  for (let letter of string){
+    switch (letter) {
+      case '1':
+        newString += 'a';
+        break;
+      case '2':
+        newString += 'e';
+        break;
+      case '3':
+        newString += 'i';
+        break;
+      case '4':
+        newString += 'o';
+        break;
+      case '5':
+          newString += 'u';
+          break;
+      default :
+      newString += letter;
+    }
+  }
+  return newString;
 
-module.exports = {sum , myRemove, myFizzBuzz};
+}
+
+
+
+
+module.exports = {sum , myRemove, myFizzBuzz, encode, decode};
--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
@@ -38,8 +38,6 @@ describe('Exercício 2', () => {
   });
 });
 
-// 
-
 describe('Exercício 3', () => {
   it ('1-Faça uma chamada com um número divisível por 3 e 5 e verifique se o retorno é o esperado', () => {
     expect(myFizzBuzz(15)).toEqual('fizzbuzz');
@@ -61,7 +59,34 @@ describe('Exercício 3', () => {
     expect(myFizzBuzz('15')).toBe(false);
 
   });
+});
+
+// 
+
+describe('Exercício 4', () => {
+  it ('1-Teste se encode e decode são funções', () => {
+    expect(typeof(encode) && typeof(encode)).toBe('function');
+  });
+
+  it ('2-Para a função encode teste se as vogais a, e, i, o, u são convertidas em 1, 2, 3, 4 e 5, respectivamente', () => {
+    expect(encode('maeioum')).toBe('m12345m');
+  });
+
+  it ('3-Para a função decode teste se os números 1, 2, 3, 4 e 5 são convertidos nas vogais a, e, i, o, u, respectivamente', () => {
+    expect(decode('12345')).toBe('aeiou');
+  });
+
+  it ('4-Teste se as demais letras/números não são convertidos para cada caso', () => {
+    expect(encode('qwrtypsdfghjklzxcvbnm') && decode('qwrtypsdfghjklzxcvbnm')).toEqual('qwrtypsdfghjklzxcvbnm');
+  });
+
+  it ('5-Teste se a string que é retornada pelas funções têm o mesmo número de caracteres que a string passada como parâmetro', () => {
+    expect(encode('teste').length && decode('teste').length).toEqual(5);
+
+  });
+
+
 
 });
 
-const {sum, myRemove, myFizzBuzz } = require('./sum.js');
+const {sum , myRemove, myFizzBuzz, encode, decode} = require('./sum.js');
