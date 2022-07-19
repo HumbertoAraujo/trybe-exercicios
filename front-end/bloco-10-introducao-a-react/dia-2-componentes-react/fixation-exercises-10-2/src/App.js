01503224e4a1cfcfc7473e537f8b058a9c53d22a
HumbertoAraujo <humbertovt@yahoo.com.br>
2022-07-19 19:20:07 -0300

    Exemplos e Exercício 1 de fixação OK

---
+++ front-end/bloco-10-introducao-a-react/dia-2-componentes-react/fixation-exercises-10-2/src/App.js
@@ -0,0 +1,149 @@
+// import React from "react";
+// import Image from "./Image";
+
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <div className="container-img">
+//       <Image source ='https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg' alternativeText='Cute cat staring' />
+//       </div>
+//     )
+//   }
+// }
+
+// export default App;
+
+// src/App.js
+// import React from 'react';
+
+// class App extends React.Component {
+//   render() {
+//     // Declaração do objeto contendo informações do album01
+//     const album01 = {
+//       image: 'https://upload.wikimedia.org/wikipedia/en/thumb/b/b0/Coldplay_-_Mylo_Xyloto.JPG/220px-Coldplay_-_Mylo_Xyloto.JPG',
+//       title: 'Mylo Xyloto',
+//       releaseDate: {
+//         year: '2011',
+//         month: '10',
+//         day: '24',
+//       },
+//       others: {
+//         recordCompany: 'Capitol, Parlophone',
+//         formats: 'CD, digital'
+//       }
+//     };
+
+//     // Declaração do objeto contendo informações do album02
+//     const album02 = {
+//       image: 'https://upload.wikimedia.org/wikipedia/pt/thumb/5/5d/Coldplay_-_Ghost_Stories.jpg/220px-Coldplay_-_Ghost_Stories.jpg',
+//       title: 'Ghost Stories',
+//       releaseDate: {
+//         year: '2014',
+//         month: '05',
+//         day: '16',
+//       },
+//       others: {
+//         recordCompany: 'Parlophone',
+//         formats: 'CD, digital'
+//       }
+//     };
+
+//     // Retorno do que será renderizado
+//     return (
+//       <article>
+//         <section>
+//           <img src={ album01.image } alt={ album01.title } />
+//           <h2>{ album01.title }</h2>
+//           <p>Lançamento: { album01.releaseDate.year }</p>
+//           <p>Gravadora: { album01.others.recordCompany }</p>
+//           <p>Formatos: { album01.others.formats }</p>
+//         </section>
+//         <section>
+//           <img src={ album02.image } alt={ album02.title } />
+//           <h2>{ album02.title }</h2>
+//           <p>Lançamento: { album02.releaseDate.year }</p>
+//           <p>Gravadora: { album02.others.recordCompany }</p>
+//           <p>Formatos: { album02.others.formats }</p>
+//         </section>
+//       </article>
+//     );
+//   }
+// }
+
+// export default App;
+
+  // src/App.js
+// import React from 'react';
+// import Album from './Album'
+
+// class App extends React.Component {
+//   render() {
+//     const album01 = {
+//       image: 'https://upload.wikimedia.org/wikipedia/en/thumb/b/b0/Coldplay_-_Mylo_Xyloto.JPG/220px-Coldplay_-_Mylo_Xyloto.JPG',
+//       title: 'Mylo Xyloto',
+//       releaseDate: {
+//         year: '2011',
+//         month: '10',
+//         day: '24',
+//       },
+//       others: {
+//         recordCompany: 'Capitol, Parlophone',
+//         formats: 'CD, digital'
+//       }
+//     };
+
+//     const album02 = {
+//       image: 'https://upload.wikimedia.org/wikipedia/pt/thumb/5/5d/Coldplay_-_Ghost_Stories.jpg/220px-Coldplay_-_Ghost_Stories.jpg',
+//       title: 'Ghost Stories',
+//       releaseDate: {
+//         year: '2014',
+//         month: '05',
+//         day: '16',
+//       },
+//       others: {
+//         recordCompany: 'Parlophone',
+//         formats: 'CD, digital'
+//       }
+//     };
+
+//     return (
+//       <div>
+//         <Album album={ album01 } />
+//         <Album album={ album02 } />
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+import React from 'react';
+import './App.css';
+import UserProfile from './UserProfile';
+
+class App extends React.Component {
+  render() {
+    const joao = {
+      id: 102,
+      name: "João",
+      email: "joao@gmail.com",
+      avatar: "https://cdn.pixabay.com/photo/2016/08/20/05/38/avatar-1606916_640.png"
+    };
+
+    const amelia = {
+      id: 77,
+      name: "Amélia",
+      email: "amelia@gmail.com",
+      avatar: "https://cdn.pixabay.com/photo/2017/01/31/13/05/cameo-2023867_640.png"
+    };
+
+    return (
+      <div className="App">
+        <UserProfile user={joao} />
+        <UserProfile user={amelia} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ front-end/bloco-10-introducao-a-react/dia-2-componentes-react/fixation-exercises-10-2/src/Image.js
@@ -0,0 +1,19 @@
+// import React from 'react';
+
+// class Image extends React.Component {
+//   render() {
+//     return <img src={this.props.source} alt={this.props.alternativeText} />;
+//   }
+// }
+
+// export default Image;
+
+import React from 'react';
+
+class Image extends React.Component {
+  render() {
+    return <img src={this.props.source} alt={this.props.alternativeText} />;
+  }
+}
+
+export default Image;
