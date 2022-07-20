bae103bafec646331b7a8c9d21f87b32e92c1364
HumbertoAraujo <humbertovt@yahoo.com.br>
2022-07-20 11:05:32 -0300

    Exercício Pokedex OK

---
+++ front-end/bloco-10-introducao-a-react/dia-2-componentes-react/my-pokedex/src/Pokedex.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Pokemon from './Pokemon';
+
+class Pokedex extends React.Component {
+  render() {
+    const { pokemons } = this.props;
+    return (
+      <section className="pokemon-list">        
+        {pokemons.map((pokemon) => (
+          <Pokemon pokemon={pokemon} key={pokemon.id} />
+        ))}
+      </section>
+    );
+  }
+}
+
+export default Pokedex;
